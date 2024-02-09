8d3565822e34f63ec5e305c33745c58f31cfbbb5
ouaatyaa <ouaatyaa@gmail.com>
2024-02-10 00:19:01 +0100

    i

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,6 @@ import Image from "next/image";
 export default function Home() {
   console.log(process.env.API_ACCESSKEY);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="border bg-blue-100 flex min-h-screen  flex-col items-center justify-between p-24"></main>
   );
 }
